717394ade135987e073da7980d646dbf993aef8a
staceylry <107966599+staceylry@users.noreply.github.com>
2024-01-17 16:48:46 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("my-button");
+btn.addEventListener("click", function(){
+    alert("You clicked me!");})
+btn.addEventListener("click", function(){
+    console.log(1);})
