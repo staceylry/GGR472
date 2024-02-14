5e0418418ccc5b820a77c10a0a345107b4eb971e
staceylry <107966599+staceylry@users.noreply.github.com>
2024-02-13 23:25:36 -0500

    lab2-2

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const btn1 = document.getElementById("lab1");
+const btn2 = document.getElementById("lab2");
 
 // btn1.addEventListener("click", function(){
 //     alert("Going to lab 1 page");})
 
-// btn.addEventListener("click", function(){
-//     console.log(1);})
-
 btn1.addEventListener("click", function() {
     window.open("lab1/index.html", "_blank");
 })
-
-// lab2
-const btn2 = document.getElementById("lab2");
 
 btn2.addEventListener("click", function() {
     window.open("lab2/lab2.html", "_blank");
